9b478570ec9427590b1ea39d87984f78ffc24f73
iMhul21 <wayne.sadiki@gmail.com>
2023-11-24 10:14:06 +0200

    Fixing vercel error pt.1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from "@/app/utils/Auth";
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as POST, handler as GET };
