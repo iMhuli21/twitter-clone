eae0e1f36d20b679a89914ff210073c57c967e4a
iMhul21 <wayne.sadiki@gmail.com>
2023-11-29 09:15:39 +0200

    Reverting changes made to auth pages

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -102,8 +102,8 @@ export default function SignIn() {
                 </div>
               </div>
               <span className="label-text-alt mt-1">
-                *Password must have atleast one uppercase, lowercase, number,
-                symbol and must be minimum of 8 characters.
+                *Password must have atleast one uppercase, lowercase, symbol and
+                must be minimum of 8 characters.
               </span>
             </div>
 
